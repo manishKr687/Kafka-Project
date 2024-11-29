ef43c6578e214a6401af467a3765c2200c74618d
Manish Kumar <manishkumar10071998@gmail.com>
2024-11-29 19:46:32 +0530

    First Commit

---
+++ updatelocation/src/main/java/com/kafkaproject/updatelocation/UpdatelocationApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaproject.updatelocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatelocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatelocationApplication.class, args);
+	}
+
+}
---
+++ updatelocation/src/test/java/com/kafkaproject/updatelocation/UpdatelocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaproject.updatelocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdatelocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
